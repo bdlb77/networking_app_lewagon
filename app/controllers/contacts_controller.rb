05ded1e4f70020c9d0400bf6c857ed9f6e802633
adi machnes glinert <amachnes@gmail.com>
2018-06-10 19:10:42 +0300

    added actions to contacts controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,22 +1,52 @@
 class ContactsController < ApplicationController
+  before_action :find_contact, only: [:show, :create, :edit,  :update, :destroy]
+
   def index
+    @contacts = Contact.all
+    # @contacts = policy_scope(Contact).order(:desc)
   end
 
   def new
+    @contact = Contact.new
+    @user = current_user
   end
 
   def show
   end
 
   def create
+     @contact.update(contact_params)
+    if @contact.save
+      redirect_to contacts_show_path(@contact)
+    else
+      render :edit
+    end
   end
 
   def edit
   end
 
   def update
+    @contact.update(contact_params)
+    if @contact.save
+      redirect_to contact_path(@event)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @contact.destroy
+    redirect_to contacts_path
   end
 end
+
+private
+
+    def find_contact
+    @contact = Contact.find(params[:id])
+  end
+
+  def contact_params
+    params.require(:contact).permit(:first_name, :last_name, :position, :company, :username, :email, :phone_number, :date_of_birth)
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,30 +1,62 @@
 Rails.application.routes.draw do
-  get 'tags/index'
-  get 'tags/new'
-  get 'tags/show'
-  get 'tags/create'
-  get 'tags/edit'
-  get 'tags/update'
-  get 'locations/index'
-  get 'locations/new'
-  get 'locations/show'
-  get 'locations/create'
-  get 'locations/edit'
-  get 'locations/update'
-  get 'milestones/index'
-  get 'milestones/new'
-  get 'milestones/show'
-  get 'milestones/create'
-  get 'milestones/edit'
-  get 'milestones/update'
-  get 'milestones/destroy'
-  get 'contacts/index'
-  get 'contacts/new'
-  get 'contacts/show'
-  get 'contacts/create'
-  get 'contacts/edit'
-  get 'contacts/update'
-  get 'contacts/destroy'
+
+  devise_for :users
+  root to: 'contacts#home'
+
+  resources :users do
+    resources :contacts, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+    resources :tags, only: [:index, :show, :new, :create, :edit, :update]
+    resources :locations, only: [:index, :show, :new, :create, :edit, :update]
+  end
+
+  # resources :users do
+  #   resources :locations, only: [:index, :show, :new, :create, :edit, :update]
+  # end
+
+  # resources :users do
+  #   resources :tags, only: [:index, :show, :new, :create, :edit, :update]
+  # end
+
+  resources :contacts, only: [:index, :show, :new, :create, :edit, :update, :destroy] do
+    resources :milestones, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  end
+
+  resources :milestones, only: [:index, :show, :new, :create, :edit, :update, :destroy] do
+    resources :tags, only: [:index, :show, :new, :create, :edit, :update]
+    resources :locations, only: [:index, :show, :new, :create, :edit, :update]
+  end
+
+  # resources :milestones, only: [:index, :show, :new, :create, :edit, :update, :destroy] do
+  #   resources :locations, only: [:index, :show, :new, :create, :edit, :update]
+  # end
+
+end
+  # get 'tags/index'
+  # get 'tags/new'
+  # get 'tags/show'
+  # get 'tags/create'
+  # get 'tags/edit'
+  # get 'tags/update'
+  # get 'locations/index'
+  # get 'locations/new'
+  # get 'locations/show'
+  # get 'locations/create'
+  # get 'locations/edit'
+  # get 'locations/update'
+  # get 'milestones/index'
+  # get 'milestones/new'
+  # get 'milestones/show'
+  # get 'milestones/create'
+  # get 'milestones/edit'
+  # get 'milestones/update'
+  # get 'milestones/destroy'
+  # get 'contacts/index'
+  # get 'contacts/new'
+  # get 'contacts/show'
+  # get 'contacts/create'
+  # get 'contacts/edit'
+  # get 'contacts/update'
+  # get 'contacts/destroy'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
