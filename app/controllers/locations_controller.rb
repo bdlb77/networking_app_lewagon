08367f34286539d27153e4b2fbe202311fc8c9e9
Ashraf Uthman <ashrafelion@gmail.com>
2018-06-10 19:40:08 +0300

    Done with the locations and tags controller

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,19 +1,47 @@
 class LocationsController < ApplicationController
+  before_action :set_milestone, only: [:create, :new, :show, :update]
+  before_action :set_user
   def index
+    @locations = Location.all
   end
 
   def new
+    @location = Location.new
   end
 
   def show
+    @location = Location.find(params[:id])
   end
 
   def create
+     @location = Location.new(location_params)
+     @location.user = current_user
+     @location.milestone = @milestone
+     @location.save
+     redirect_to milestone_locations_path
   end
 
   def edit
   end
 
   def update
+    @location = Location.find(params[:id])
+    if @location.save
+      @location.update(location_params)
+      redirect_to milestone_locations_path
+    else
+      render :edit
+    end
+  end
+   private
+
+  def set_milestone
+    @milestone = Milestone.find(params[:milestone_id])
+  end
+  def location_params
+    params.require(:location).permit(:title)
+  end
+  def set_user
+    @user = current_user
   end
 end
--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,19 +1,49 @@
 class TagsController < ApplicationController
+  before_action :set_milestone, only: [:create, :new, :show, :update]
+  before_action :set_user
   def index
+    @tags = Tag.all
+
   end
 
   def new
+    @tag = Tag.new
   end
 
   def show
+    @tag = Tag.find(params[:id])
   end
 
   def create
+    @tag = Tag.new(tag_params)
+    @tag.user = current_user
+    @tag.milestone = @milestone
+    @tag.save
+    redirect_to milestone_tags_path
   end
 
   def edit
   end
 
   def update
+   @tag = Tag.find(params[:id])
+  if @tag.save
+    @tag.update(tag_params)
+    redirect_to milestone_tags_path
+  else
+    render :edit
+  end
+  end
+
+  private
+
+  def set_milestone
+    @milestone = Milestone.find(params[:milestone_id])
+  end
+  def tag_params
+    params.require(:tag).permit(:title)
+  end
+  def set_user
+    @user = current_user
   end
 end
