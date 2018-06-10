0d12cf4b9e777cb40813189e83d7bdd0b66ac705
Bryan Leighton <bryan.leigh77@gmail.com>
2018-06-10 19:29:24 +0300

    added milestones controller actions

--- app/controllers/milestones_controller.rb
+++ app/controllers/milestones_controller.rb
@@ -1,22 +1,62 @@
 class MilestonesController < ApplicationController
+    before_action :set_milestone, only: [:show]
+    before_action :set_contact, only: [:create]
   def index
+    @milestones = Milestone.all
   end
 
   def new
+    @milestone = Milestone.new
   end
 
-  def show
-  end
 
   def create
+    @milestone = Milestone.new
+    @milestone.user = current_user
+    @milestone.contact = @contact
+    if @milestone.save
+      flash[:alert] = " Your Milestone has been set!"
+      redirect_to user_milestones_path
+    else
+      render: new
+    end
   end
 
   def edit
   end
 
   def update
+    @milestone
+    if @milestone.save
+      @milestone.update(milestone_params)
+      flash[:alert] = " Your Milestone has been updated!"
+      redirect_to user_milestones_path
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @milestone.destroy
+    flash[:alert] = "Your Milestone for #{@milestone.contact.first_name} 
+      has been deleted!"
   end
+  
+  def show
+  end
+
+  private
+
+  def set_contact
+    @contact = Contact.find(params[:contact_id])    
+  end
+
+  def set_milestone
+    @milestone = Milestone.find(params[:id])
+  end
+
+  def milestone_params
+    params.require(:milestone).permit(:notes, :contact_type)      
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+require 'faker'
+Contact.destroy_all
+Milestone.destroy_all
+User.destroy_all
+
+user = User.new(
+	email: 'bdlb@utexas.edu',
+	password: 123456
+)
+user.save! 
+ 
+	contact = Contact.new(
+		first_name: 'Bryan',
+		user_id: user.id
+	)
+	contact.save!
+
+
+puts "finished!"
+
+  milestone = Milestone.new(
+    id: 1,
+    contact_type: Faker::Address.full_address,
+    notes: Faker::Friends.quote,
+    contact_id: contact.id
+  )
+  milestone.save!
+
+
