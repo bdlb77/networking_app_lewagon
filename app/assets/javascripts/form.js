ce6de054aefa77064788e5943afd4fb1d061fedf
Ashraf Uthman <ashrafelion@gmail.com>
2018-06-16 22:26:16 +0300

    Contact show paths

--- app/assets/javascripts/form.js
+++ app/assets/javascripts/form.js
@@ -1,25 +1,25 @@
 
-Array.from(document.getElementById('form_s').children).forEach(function(option){
-  if(option.value == 'show'){
-    option.addEventListener('click', function(){
-      document.getElementById('location_dropdown').style.display = 'block';
-    })
+// Array.from(document.getElementById('form_s').children).forEach(function(option){
+//   if(option.value == 'show'){
+//     option.addEventListener('click', function(){
+//       document.getElementById('location_dropdown').style.display = 'block';
+//     })
 
-  } else{
-     option.addEventListener('click', function(){
-      document.getElementById('location_dropdown').style.display = 'none';
-    })
+//   } else{
+//      option.addEventListener('click', function(){
+//       document.getElementById('location_dropdown').style.display = 'none';
+//     })
 
-  }
-});
+//   }
+// });
 
 
-document.getElementById('newbutton').addEventListener('click', function(){
-  if(document.getElementById('form_s').options[document.getElementById('form_s').selectedIndex].value == 'Add another location'){
+// document.getElementById('newbutton').addEventListener('click', function(){
+//   if(document.getElementById('form_s').options[document.getElementById('form_s').selectedIndex].value == 'Add another location'){
 
-  }
-  else{
-    document.getElementById('location_dropdown').value = document.getElementById('form_s').options[document.getElementById('form_s').selectedIndex].value
-  }
-  document.getElementById('submit_button').click()
-});
+//   }
+//   else{
+//     document.getElementById('location_dropdown').value = document.getElementById('form_s').options[document.getElementById('form_s').selectedIndex].value
+//   }
+//   document.getElementById('submit_button').click()
+// });
