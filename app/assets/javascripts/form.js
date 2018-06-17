46362c574f70eed7cbbc532e787655754e8923a3
Ashraf Uthman <ashrafelion@gmail.com>
2018-06-17 10:14:53 +0300

    conflict solved

--- app/assets/javascripts/form.js
+++ app/assets/javascripts/form.js
@@ -1,25 +1,33 @@
 
-// Array.from(document.getElementById('form_s').children).forEach(function(option){
-//   if(option.value == 'show'){
-//     option.addEventListener('click', function(){
-//       document.getElementById('location_dropdown').style.display = 'block';
-//     })
 
-//   } else{
-//      option.addEventListener('click', function(){
-//       document.getElementById('location_dropdown').style.display = 'none';
-//     })
+if(document.location.pathname == "/contacts/new"){
+  console.log("IT worked!");
+Array.from(document.getElementById('form_s').children).forEach(function(option){
+  if(option.value == 'show'){
+    option.addEventListener('click', function(){
+      console.log("1");
+      document.getElementById('location_dropdown').style.display = 'block';
+    })
 
-//   }
-// });
+  } else{
+     option.addEventListener('click', function(){
+      console.log("2");
+      document.getElementById('location_dropdown').style.display = 'none';
+    })
+
+  }
+});
 
 
-// document.getElementById('newbutton').addEventListener('click', function(){
-//   if(document.getElementById('form_s').options[document.getElementById('form_s').selectedIndex].value == 'Add another location'){
+document.getElementById('newbutton').addEventListener('click', function(){
+  if(document.getElementById('form_s').options[document.getElementById('form_s').selectedIndex].value == 'Add another location'){
 
-//   }
-//   else{
-//     document.getElementById('location_dropdown').value = document.getElementById('form_s').options[document.getElementById('form_s').selectedIndex].value
-//   }
-//   document.getElementById('submit_button').click()
-// });
+  }
+  else{
+    document.getElementById('location_dropdown').value = document.getElementById('form_s').options[document.getElementById('form_s').selectedIndex].value
+  }
+  document.getElementById('submit_button').click()
+});
+
+
+}
