cdd60948f1ddeda72c6b43d63300b3934e62d967
Ashraf Uthman <ashrafelion@gmail.com>
2018-06-19 21:00:15 +0300

    Charts added

--- app/assets/javascripts/milestones.js
+++ app/assets/javascripts/milestones.js
@@ -30,31 +30,28 @@
   window.addEventListener("resize", callbackFunc);
   window.addEventListener("scroll", callbackFunc);
 
-// var data = {
-//   labels: ['Bananas', 'Apples', 'Grapes'],
-//   series: [20, 15, 40]
-// };
+// pie chart
 
-// var options = {
-//   labelInterpolationFnc: function(value) {
-//   return value[0]
-// }
-// };
+var options = {
+  labelInterpolationFnc: function(value) {
+  return value[0]
+}
+};
 
-// var responsiveOptions = [
-//   ['screen and (min-width: 640px)', {
-//   chartPadding: 30,
-//   labelOffset: 100,
-//   labelDirection: 'explode',
-//   labelInterpolationFnc: function(value) {
-//   return value;
-// }
-// }],
-// ['screen and (min-width: 1024px)', {
-// labelOffset: 80,
-// chartPadding: 20
-// }]
-// ];
+var responsiveOptions = [
+  ['screen and (min-width: 640px)', {
+  chartPadding: 50,
+  labelOffset: 20,
+  labelDirection: 'explode',
+  labelInterpolationFnc: function(value) {
+  return value;
+}
+}],
+['screen and (min-width: 1024px)', {
+labelOffset: 40,
+chartPadding: 20
+}]
+];
 
-// new Chartist.Pie('.ct-chart', data, options, responsiveOptions);
+new Chartist.Pie('.ct-chart', milestoneChartData, options, responsiveOptions);
 })();
